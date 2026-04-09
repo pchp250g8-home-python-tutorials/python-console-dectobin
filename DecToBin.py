b6d42561a6cddf352ddc17755fdc11b1965e56bb
Alexander <pchp250g8@tutamail.com>
2026-04-09 17:49:42 +0300

    Fixed Source Code

--- DecToBin.py
+++ DecToBin.py
@@ -1,17 +1,17 @@
-#--conding:utf-8--
-import math
+# --conding:utf-8--
 import os
 
 os.system("cls")
 print("Input an unsigned integer number:")
+MAX_INT = 2 ** 32 - 1
 nDecNum = int(input())
 strLine = ""
-if(nDecNum < 0):
+if (nDecNum < 0 or nDecNum > MAX_INT):
     print("Invalid number format")
 nTempVal = nDecNum
 while (nTempVal > 0):
-    strLine = str(nTempVal % 2)  + strLine
+    strLine = str(nTempVal % 2) + strLine
     nTempVal //= 2
-if(len(strLine) == 0):
+if (len(strLine) == 0):
     strLine = "0"
-print(f"The binary equivalent of the decenary number {nDecNum} is {strLine}")
+print(f"The binary equivalent of the decimal number {nDecNum} is {strLine}")
